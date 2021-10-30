443e2d04d1533524e62289036dc3d28078090b84
amc644 <amc644@gmail.com>
2021-10-30 12:47:59 -0500

    se conectó signUp con la base de datos

--- app.py
+++ app.py
@@ -2,9 +2,10 @@
 import os
 
 #Importamos la librería Flask,
-from flask import Flask
+from flask import Flask,request
 from flask.templating import render_template
 from forms import wtfLogIn, wtfResetPassword, wtfSendPassword, wtfSignUp
+from models import patients
 
 #Creamos un ejemplar de tipo Flask
 app=Flask(__name__)
@@ -19,21 +20,44 @@ app.config['SECRET_KEY']= os.urandom(32)
 def wpHome():
     return render_template("home.html")
 
+@app.route("/signUp/",methods=['GET','POST'])
+def wpSignUp():
+    form=wtfSignUp(request.form)
+    if form.validate_on_submit():# si el formulario esta bien
+        newPatient=patients(
+                                id=0,
+                                fName=form.firstName.data,
+                                mName="",
+                                lName=form.lastName.data,
+                                cPhone="",
+                                hPhone="",
+                                wPhone="",
+                                email=form.email.data,
+                                adr1="",
+                                adr2="",
+                                city="",
+                                state="",
+                                zc="",
+                                ssn="",
+                                dob="",
+                                usrnm=form.username.data,
+                                pw=form.password.data  
+                            )
+        if newPatient.insert():
+            print("SE INGRESO A LA BASE DE DATOS")
+            return render_template("patient.html")
+        else:
+            print("NO SE INGRESO NADA")
+            return render_template("signUp.html",form=form)
+    return render_template("signUp.html",form=form)
+
 @app.route("/logIn/")
 def wpLogIn():
-    fLogIn=wtfLogIn()
-    return render_template("logIn.html",wtfLogIn=fLogIn)
-
-@app.route("/signUp/")
-def wpSignUp():
-    fSignUp=wtfSignUp()
-    return render_template("signUp.html",wtfSignUp=fSignUp)
+    return render_template("logIn.html")
 
 @app.route("/resetPassword/")
 def wpResetPassword():
-    fSendPassword=wtfSendPassword()
-    fResetPassword=wtfResetPassword()
-    return render_template("resetPassword.html",wtfSendPassword=fSendPassword,wtfResetPassword=fResetPassword)
+    return render_template("resetPassword.html")
 
 @app.route("/patient/")
 def wpPatient():
